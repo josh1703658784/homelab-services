cea4784b27d00b13d627c9dca27b4dfb2379a738
joshuaks <5505155+joshuaks@users.noreply.github.com>
2023-02-22 01:26:35 -0800

    apply stash.

---
+++ tower/dependencies/tv/openvpn-pre-start.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+
+# optional shellcheck options
+# shellcheck enable=add-default-case
+# shellcheck enable=avoid-nullary-conditions
+# shellcheck enable=check-unassigned-uppercase
+# shellcheck enable=deprecate-which
+# shellcheck enable=quote-safe-variables
+# shellcheck enable=require-variable-braces
+
+
+# ENV SETTINGS ##########
+set -e                      # exit all shells if script fails
+set -u                      # exit script if uninitialized variable is used
+set -o pipefail             # exit script if anything fails in pipe
+shopt -s failglob           # fail on regex expansion fail
+shopt -s nullglob           # enables recursive globbing
+IFS=$'\n\t'
+
+
+# MAIN ##################
+function main(){
+
+  local -r src='/readonly_vpn_config.ovpn'
+  local -r dest='/etc/openvpn/custom/vpn_config.ovpn'
+  local -r dest_dirpath="$( dirname "${dest}" )"
+    
+  mkdir -p "${dest_dirpath}"
+  cp "${src}" "${dest}"
+  chmod 666 "${dest}"
+
+  exit 0
+}
+main
+
