00b0de8863c3f4d7e851356fb4fedccb810ee6c7
joshuaks <5505155+joshuaks@users.noreply.github.com>
2023-08-18 02:17:48 -0700

    * commented out tailscale, exit-node, proxy, whoami and moved into ./caddy/docker-compose.yml while developing
    * created a .env and added to vars to ./docker-compose.yml
    
    * started on building a custom caddy image with an experimental tailscale integration plugin
    '

--- nas/init_docker_env.sh
+++ nas/init_docker_env.sh
@@ -13,7 +13,11 @@ set -u                      # exit script if uninitialized variable is used
 set -o pipefail             # exit script if anything fails in pipe
 
 function main(){
-  docker context use NAS
+  docker context use NAS_INTERNAL
   docker context list
+
+  # https://github.com/lucaslorentz/caddy-docker-proxy#basic-usage-example-using-docker-compose
+  # this network must be created external to the docker-compose file
+  docker network create caddy
 }
 main
