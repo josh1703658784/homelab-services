1a61f2a48eb7bcc5db5d8641970a6f02d83c43c0
joshuaks <5505155+joshuaks@users.noreply.github.com>
2023-10-20 01:36:07 -0700

    radarr and sonarr were not able to create hardlinks and thus terabytes of duplicate data was created. this is the first step in that process in trying to resolve it to bring all related directories under one parent directory.

--- nas/script.sh
+++
@@ -1,7 +0,0 @@
-
-readonly REMOVE_ME="$(docker volume ls | grep 'local     caddy_tailscale-sidecar\|local     nas_' | awk '{{ print $2 }}' )"
-
-
-for name in ${REMOVE_ME}; do
-  docker volume rm "${name}"
-done
